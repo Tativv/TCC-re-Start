12b95ee29a19ca6ce578a9a5c6024d6dbc5ffc90
Tativv <varonatatiana1@gmail.com>
2023-12-19 20:11:07 -0300

    upload img S3

--- assets/app.js
+++ assets/app.js
@@ -1,51 +1,88 @@
-const dropArea = document.getElementById("drop-area");
 const inputFile = document.getElementById("input-file");
-const imageView = document.getElementById("img-view");
+const uploadBtn = document.getElementById("upload-btn");
+const imgView = document.getElementById("img-view");
+const dropArea = document.getElementById("drop-area")
 
-inputFile.addEventListener("change", uploadImage);
 
-    function uploadImage() {
-        const fileInput = document.getElementById('fileInput');
-        const file = fileInput.files[0];
+// Configura tus credenciales de AWS
+AWS.config.update({
+    accessKeyId: 'AKIAQZVZOJ5MFWWDTGN7',
+    secretAccessKey: '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',
+    region: 'sa-east-1'
+});
 
-        if (!file) {
-        alert('Selecciona una imagen antes de subir.');
+
+const s3 = new AWS.S3();
+
+function uploadImage() {
+    const file = inputFile.files[0];
+    if (!file) {
+        alert('Selecione um arquivo');
         return;
-        }
+    }
 
-        // Crea un FormData para enviar la imagen a la función Lambda
-        const formData = new FormData();
-        formData.append('file', file);
-    
-        // Realiza una solicitud HTTP a tu función Lambda
-        fetch('URL_DE_TU_API_GATEWAY', {
-        method: 'POST',
-        body: formData,
-        })
-        .then(response => {
-        if (!response.ok) {
-            throw new Error('Error al subir la imagen');
-        }
-        return response.json();
-        })
-        .then(data => {
-        console.log('Imagen subida con éxito:', data);
-        alert('Imagen subida con éxito.');
-        })
-        .catch(error => {
-        console.error('Error al subir la imagen:', error);
-        alert('Error al subir la imagen.');
-        });
+    // Especifica la carpeta dentro del bucket
+    const folder = 'imagens/';
+    const params = {
+        Bucket: 'grupo1-restart-brpor1',
+        Key: folder + file.name,
+        ContentType: file.type,
+        Body: file,
+        ACL: 'public-read' // Opcional: establece los permisos del archivo
     };
 
+    s3.upload(params, function(err, data) {
+        if (err) {
+            console.error('Erro ao enviar o arquivo:', err);
+            alert('Erro ao enviar o arquivo');
+        } else {
+            alert('Arquivo enviado com sucesso');
+            console.log(data);
 
-dropArea.addEventListener("dragover",function(e){
+            // Limpiar la entrada de archivos y la vista previa después de la carga
+            inputFile.value = "";
+            imgView.style.backgroundImage = 'none';
+            imgView.innerHTML = '<img src="assets/img/cloud-upload-regular-180.png" alt=""><p>Arraste e solte <br> ou <br>clique aqui para carregar a imagem</p><span>Carregue qualquer imagem do computador</span>';
+        }
+    });
+}
+
+
+dropArea.addEventListener("dragover", function(e){
     e.preventDefault();
 });
 
 dropArea.addEventListener("drop", function(e){
     e.preventDefault();
-    inputFile.files = e.dataTransfer.files;
-    uploadImage();
-}); 
+    const files  = e.dataTransfer.files;
+    if (files.length > 0) {
+        // Actualizar la vista previa al soltar la imagen
+        const imgLink = URL.createObjectURL(files[0]);
+        imgView.style.backgroundImage = `url(${imgLink})`;
+    imgView.textContent = "";
+    imgView.style.border = 0;
+
+        // Asignar el archivo al input para que esté disponible para la carga posterior
+        inputFile.files = files;
+    }    
+});
+
+function fileSelected(event) {
+    const file = event.target.files[0];
+    if (!file) {
+        alert('Seleccione un archivo');
+        return;
+    }
+
+    // Actualizar la vista previa al seleccionar un archivo
+    const imgLink = URL.createObjectURL(file);
+    imgView.style.backgroundImage = `url(${imgLink})`;
+    imgView.textContent = "";
+    imgView.style.border = 0;
+}
+
+
+
+
+
 
